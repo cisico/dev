73da00eea6d15b043139db2e05741714a3df5d8a
cisico <50976844@qq.com>
2018-09-13 19:41:41 +0800

    a

--- src/main/java/com/hf/mongo/Mongotest.java
+++ src/main/java/com/hf/mongo/Mongotest.java
@@ -8,5 +8,6 @@ public class Mongotest {
     public static void main(String[] args) {
         System.out.println("xxxxxx");
 
+
     }
 }
--- src/main/java/com/hf/mongo/T1.java
+++
@@ -1,8 +0,0 @@
-package com.hf.mongo;
-
-/**
- * @author zhaihui
- * @date 2018/8/23
- */
-public class T1 {
-}
