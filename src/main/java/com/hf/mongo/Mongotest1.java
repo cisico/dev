fa6f693027ad51500cb28c69892bfcf332483333
cisico <50976844@qq.com>
2018-09-19 09:33:32 +0800

    test

--- src/main/java/com/hf/mongo/Mongotest1.java
+++ src/main/java/com/hf/mongo/Mongotest1.java
@@ -4,10 +4,10 @@ package com.hf.mongo;
  * @author zhaihui
  * @date 2017/11/15
  */
-public class Mongotest {
+public class Mongotest1 {
     public static void main(String[] args) {
         System.out.println("xxxxxx");
 
-
+        System.out.println("xxxxx");
     }
 }
--- src/main/java/com/hf/mongo/T4.java
+++ src/main/java/com/hf/mongo/T4.java
@@ -2,9 +2,7 @@ package com.hf.mongo;
 
 /**
  * @author zhaihui
- * @date 2018/8/23
+ * @date 2018/9/17
  */
-public class T2 {
-
-
+public class T4 {
 }
